b74ae023f4b41fea70dd19f422d64ba49c0869b2
abram-stack <anthonybramantyo@outlook.com>
2021-12-10 17:48:25 +0100

    add header

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+    next(); // Important
+})
+
 // getDatabaseNames return a promise
 client.getDatabaseNames()
   .then(names => {
