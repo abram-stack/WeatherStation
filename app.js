c26eb64436d28cf4cdd93501220df9555e8f45d3
abram-stack <anthonybramantyo@outlook.com>
2021-12-12 12:18:56 +0100

    dummy data for humidity station0 sensor201

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
-  next(); // Important
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET , PUT , POST , DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, x-requested-with");
+//   next(); // Important
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
